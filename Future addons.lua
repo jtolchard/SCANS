2c2fe1b7300b2b452225fa24fd1fa4b40b92726d
James Tolchard <james.tolchard@univ-lyon1.fr>
2023-06-05 16:19:28 +0200

    ideas file

---
+++ Future addons.lua
@@ -0,0 +1,12 @@
+Future addons
+
+Define sets of grok config files for MICS & non-MICS systems
+
+Import from TopSpin
+-- topspin version
+-- Shim coil temperature
+-- MAS speed
+-- MAS gas (bearing / drive / Main (internal/external))
+-- VTU Sample temp / probe heater % / gas flow / bearing + drive (temp + target temp)
+-- exp queue time
+-- Status (autoshim / locked / error)
